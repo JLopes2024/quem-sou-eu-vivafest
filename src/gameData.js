6515d0b3c002924fb24740f73faa5d04b7b22d70
JLopes2024 <jeffinho.je8@gmail.com>
2026-09-22 04:20:32 -0300

    arquivos-iniciais

---
+++ src/gameData.js
@@ -0,0 +1,12 @@
+export const gameItems = [
+  "Atestado de vacina que não é válido na CLT",
+  "Um cafezin",
+  "Ar-condicionado quebrado em 17 °C",
+  "Fone de ouvido quebrado de um lado",
+  "Post-it que não gruda",
+  "A hora de ir embora",
+  "Reunião que poderia ter sido um e-mail",
+  "Wi-Fi que só funciona perto da porta",
+  "Impressora com papel, mas sem tinta",
+  "Crachá esquecido em casa",
+];
